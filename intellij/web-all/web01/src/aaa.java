2c5997b7e1412f7377230379c4319d34ec10c778
lyl <13416088820@163.com>
2025-05-04 20:39:31 +0800

    5-4

---
+++ intellij/web-all/web01/src/aaa.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName: aaa
+ * Package: PACKAGE_NAME
+ * Description:创建于 2025/5/4 19:18
+ *
+ * @Author lyl
+ * @Version 1.0
+ */
+public class aaa {
+    public void bbb(){
+
+    }
+}
