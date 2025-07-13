63bfb4162cca3507b8532f2dea31a436f3391625
lyl <13416088820@163.com>
2025-07-13 23:12:03 +0800

    7-13

---
+++ vscode/demo12-axios/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import axios from 'axios'
+import {ref} from 'vue'
+
+let message=ref("土味情话")
+
+function getMessage(){
+  axios({
+    method:"get",
+    url:"https://api.uomg.com/api/rand.qinghua?format=json"
+    //请求体中内容：data:
+    //路径参数内容：params:
+  }).then(
+    (Response)=>{
+      console.log(Response);
+      message.value=Response.data.content
+    }
+  ).catch(
+    (error)=>{
+      console.log(error);
+    }
+  )
+
+}
+</script>
+
+<template>
+  <div>
+    <h3 v-text="message"></h3>
+    <hr>
+    <button @click="getMessage()">获取土味情话</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
